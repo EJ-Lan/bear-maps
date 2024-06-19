8f76ee932c2929e2dfa13cec78523990fc4269e5
Tioluwani <43251329+t1oluwani@users.noreply.github.com>
2024-06-18 21:30:39 -0600

    Prettier dependencies and basic set up

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # web-group
+
 Web Project
--- frontend/plopfile.js
+++ frontend/plopfile.js
@@ -1,31 +1,30 @@
 module.exports = function (plop) {
-    
-    // Component generator
-    plop.setGenerator('component', {
-        description: 'Create a new component with CSS, README, and React files',
-        prompts: [
-            {
-                type: 'input',
-                name: 'name',
-                message: 'Component name:',
-            },
-        ],
-        actions: [
-            {
-                type: 'add',
-                path: 'src/components/{{pascalCase name}}/{{pascalCase name}}.jsx',
-                templateFile: 'plop-templates/Component.jsx.hbs',
-            },
-            {
-                type: 'add',
-                path: 'src/components/{{pascalCase name}}/{{pascalCase name}}.css',
-                templateFile: 'plop-templates/Component.css.hbs',
-            },
-            {
-                type: 'add',
-                path: 'src/components/{{pascalCase name}}/README.md',
-                templateFile: 'plop-templates/README.md.hbs',
-            },
-        ],
-    });
+  // Component generator
+  plop.setGenerator("component", {
+    description: "Create a new component with CSS, README, and React files",
+    prompts: [
+      {
+        type: "input",
+        name: "name",
+        message: "Component name:",
+      },
+    ],
+    actions: [
+      {
+        type: "add",
+        path: "src/components/{{pascalCase name}}/{{pascalCase name}}.jsx",
+        templateFile: "plop-templates/Component.jsx.hbs",
+      },
+      {
+        type: "add",
+        path: "src/components/{{pascalCase name}}/{{pascalCase name}}.css",
+        templateFile: "plop-templates/Component.css.hbs",
+      },
+      {
+        type: "add",
+        path: "src/components/{{pascalCase name}}/README.md",
+        templateFile: "plop-templates/README.md.hbs",
+      },
+    ],
+  });
 };
