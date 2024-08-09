915f318bf37f936a594fb548d6ae91a41938ed01
Tioluwani <43251329+t1oluwani@users.noreply.github.com>
2024-08-09 15:07:14 -0600

    clean up and img positioning

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import './Login.css';
+import Logo from '../assets/bearmaps_logo.png';
 import Friends from '../assets/friends_studying.png';
 
 function LoginPage() {
     return (
         <div className="Login_Page">
-            
+
             <div className="Right">
                 <img src={Friends} alt="Friends Studying" />
             </div>
 
             <div className='Left'>
+                <img src={Logo} alt="BearMaps Logo" />
+
                 <h1>BearMaps</h1>
                 <h3>For UofA Students by UofA Students, find your study spot now!</h3>
 
@@ -21,11 +24,15 @@ function LoginPage() {
 
 
                 <form>
-                    <p className="EmailUalbertaDomain">Email  (ualberta domain)</p>
-                    <input type="text" className="email" placeholder='tgakinlo@ualberta.ca' />
+                    <div className='input_rectangle'>
+                        <p>Email (ualberta domain)</p>
+                        <input type="text" className="email" placeholder='tgakinlo@ualberta.ca' />
+                    </div>
 
-                    <p className="Password">Password</p>
-                    <input type="password" className="password" placeholder='*******************' />
+                    <div className='input_rectangle'>
+                        <p>Password</p>
+                        <input type="password" className="password" placeholder='*******************' />
+                    </div>
 
                     <button className="Login">Login</button>
                     <button className="SignUp">Sign Up</button>
