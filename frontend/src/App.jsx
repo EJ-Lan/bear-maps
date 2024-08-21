8dcca1cf3f4a369601cedbc013042c8cca2d7439
Tioluwani <43251329+t1oluwani@users.noreply.github.com>
2024-08-21 16:04:14 -0600

    Forgot Password initial ver, no space styling

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,6 +8,7 @@ import StudySpotAdder from "./components/StudySpotAdder.jsx";
 
 import LoginPage from "./components/Login.jsx";
 import SignUpPage from "./components/SignUp.jsx";
+import ForgotPasswordPage from "./components/ForgotPassword.jsx";
 import StudySessionFinder from "./components/ActiveStudySessions.jsx"; 
 import MajorFilter from "./components/StudentMajorFilter.jsx";
 
@@ -16,9 +17,11 @@ import MajorFilter from "./components/StudentMajorFilter.jsx";
 function App() {
   return (
     <>
-      <Navbar />
+      {/* <Navbar />   */}
 
       {/* For Testing Page Components */}
+
+      <ForgotPasswordPage />
 
       {/* <StudySpotAdder /> */}
 
