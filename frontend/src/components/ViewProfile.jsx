92a0f84efe1d4573468e2b8e1909c5622fb74b85
Tioluwani <43251329+t1oluwani@users.noreply.github.com>
2024-08-21 20:50:57 -0600

    checkpoint

--- frontend/src/components/ViewProfile.jsx
+++ frontend/src/components/ViewProfile.jsx
@@ -5,38 +5,44 @@ import Logo from '../assets/bearmaps_logo.png';
 function ViewProfilePage() {
   return (
     <div className={styles.ViewProfilePage}>
-      <img src={Logo} alt="BearMaps Logo" />
+      <img className={styles.Bearmaps_Logo} src={Logo} alt="BearMaps Logo" />
 
       <div className={styles.Center}>
 
         <div className={styles.Profile_Container}>
 
-          <img className='Profile_Picture' src='https://www.pngkey.com/png/full/114-1149878_setting-user-avatar-in-specific-size-without-breaking.png' alt='Profile Picture' />
-          <h3> FirstName LastName</h3>
+          <img className={styles.Profile_Picture} src='https://www.pngkey.com/png/full/114-1149878_setting-user-avatar-in-specific-size-without-breaking.png' alt='Profile Picture' />
+          <h2 className={styles.name}>FirstName LastName</h2>
 
-          <div className='Profile_Content'>
-            <h4 className='email'> Email </h4>
-            <h4 className='degree'> Degree </h4>
-            <h4 className='year'> Year </h4>
-            <h4 className='bio'> Bio </h4>
+          <div className={styles.Profile_Content}>
+            <h4 className={styles.email}> Email </h4>
+            <h4 className={styles.degree}> Degree </h4>
+            <h4 className={styles.year}> Year </h4>
+            <h4 className={styles.bio}> Bio </h4>
 
-            <label for="dropdown1">Courses Enrolled</label>
-            <select id="dropdown1" name="courses">
-              <option value="course1">Option 1</option>
-              <option value="course2">Option 2</option>
-              <option value="course3">Option 3</option>
-            </select>
+            <div className={styles.courses}>
+              <label for="dropdown1">Courses Enrolled</label>
+              <select id="dropdown1" name="courses">
+                <option value="course1">Option 1</option>
+                <option value="course2">Option 2</option>
+                <option value="course3">Option 3</option>
+              </select>
+            </div>
 
-            <h4 className='current'>Current Study Spot/Course Studying</h4>
+            <h4 className={styles.current}>Current Study Spot/Course Studying</h4>
 
-            <label for="dropdown2">Study Sessions Enrolled</label>
-            <select id="dropdown2" name="studysessions">
-              <option value="studysesh1">Option 1</option>
-              <option value="studysesh2">Option 2</option>
-              <option value="studysesh3">Option 3</option>
-            </select>
+            <div className={styles.studysesh}>
+              <label for="dropdown2">Study Sessions Enrolled</label>
+              <select id="dropdown2" name="studysessions">
+                <option value="studysesh1">Option 1</option>
+                <option value="studysesh2">Option 2</option>
+                <option value="studysesh3">Option 3</option>
+              </select>
+            </div>
 
           </div>
+
+          <button className={styles.message}>Message</button>
 
         </div>
       </div>
