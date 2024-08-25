fb23686f56e726ef745103b1682db13bd6b2bfb0
Tioluwani <43251329+t1oluwani@users.noreply.github.com>
2024-08-25 00:53:07 -0600

    class and detail changes

--- frontend/src/components/EditProfile.jsx
+++ frontend/src/components/EditProfile.jsx
@@ -12,22 +12,19 @@ function EditProfilePage() {
         <div className={styles.Profile_Container}>
 
           <img className={styles.Profile_Picture} src='https://www.pngkey.com/png/full/114-1149878_setting-user-avatar-in-specific-size-without-breaking.png' alt='Profile Picture' />
-          <h2 className={styles.name}>First Name Last Name</h2>
+          <h2 className={styles.photo}>Edit Photo</h2>
 
           <div className={styles.Profile_Content}>
 
             <div className={styles.Profile_Info}>
-              <div className={styles.leftSection}>
-                <h4 className={styles.email}> Email </h4>
-                <h4 className={styles.degree}> Degree </h4>
-                <h4 className={styles.year}> Year </h4>
-              </div>
-
-              <h4 className={styles.bio}> Bio </h4>
+              <h4 className={styles.name}> Name </h4>
+              <h4 className={styles.email}> Email </h4>
+              <h4 className={styles.degree}> Degree </h4>
+              <h4 className={styles.year}> Year of Degree </h4>
             </div>
 
             <div className={styles.courses}>
-              <label htmlFor="dropdown1">Courses Enrolled</label>
+              <label htmlFor="dropdown1">Courses</label>
               <select id="dropdown1" name="courses">
                 <option value="course1">Option 1</option>
                 <option value="course2">Option 2</option>
@@ -35,27 +32,9 @@ function EditProfilePage() {
               </select>
             </div>
 
-            <div className={styles.current}>
-              <div className={styles.circles}>
-                <div className={styles.circle1}></div>
-                <div className={styles.circle2}></div>
-                <div className={styles.circle3}></div>
-              </div>
-              <h4>Current Study Spot/Course Studying</h4>
-            </div>
-
-            <div className={styles.studysesh}>
-              <label htmlFor="dropdown2">Study Sessions Enrolled</label>
-              <select id="dropdown2" name="studysessions">
-                <option value="studysesh1">Option 1</option>
-                <option value="studysesh2">Option 2</option>
-                <option value="studysesh3">Option 3</option>
-              </select>
-            </div>
-
           </div>
 
-          <button className={styles.message}>Message</button>
+          <button className={styles.submit}>Submit</button>
 
         </div>
       </div>
